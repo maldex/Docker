d2f522f8e4f1f29c554ee71734f8187a2123aef9
default system login user <devoschy@poseidix.bnet>
2021-12-05 00:55:39 +0100

    alpine issue!

--- apache2_mod_evasive_proxy_ssl/run.sh
+++ apache2_mod_evasive_proxy_ssl/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "\t\t\t]]]]]]]] learning user and group from httpd.conf [[[[[[[["
+echo -e "$0\t\t\t]]]]]]]] learning user and group from httpd.conf [[[[[[[["
 user=`sed -n -e 's/^User \(.*\)/\1/p' /etc/httpd/conf/httpd.conf`
 group=`sed -n -e 's/^User \(.*\)/\1/p' /etc/httpd/conf/httpd.conf`
 
@@ -8,17 +8,20 @@ echo -en "user '${user}'\t\t"; getent passwd ${user}
 echo -en "group '${group}'\t\t"; getent group ${group}
 echo -e "hostname\t\t${HOSTNAME}"
 
-echo -e "\t\t\t]]]]]]]] adjusting ServerName [[[[[[[["
+echo -e "$0\t\t\t]]]]]]]] adjusting ServerName [[[[[[[["
 sed -i 's/^.#ServerName .*/ServerName ${HOSTNAME}/g' /etc/httpd/conf/httpd.conf
 sed -i 's/{HOSTNAME}/'${HOSTNAME}'/g' /var/www/html/index.html
 
 
-echo -e "\t\t\t]]]]]]]] list apache modules [[[[[[[["
+echo -e "$0\t\t\t]]]]]]]] list apache modules [[[[[[[["
 httpd -M
 
-echo -e "\t\t\t]]]]]]]] list apache configuration [[[[[[[["
+echo -e "$0\t\t\t]]]]]]]] list apache configuration [[[[[[[["
 httpd -S 
 
-echo -e "\t\t\t]]]]]]]] starting apache [[[[[[[["
+echo -e "$0\t\t\t]]]]]]]] starting apache [[[[[[[["
 httpd -D FOREGROUND 
+retval=$?
+echo -e "$0\t\t\t]]]]]]]] exit with ${retval} [[[[[[[["
+
 exit $?
